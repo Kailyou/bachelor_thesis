09d492ea684cf28c46ccc1bd364bd7144c174e8e
Kailyou <maik.thielen@gmail.com>
2019-05-21 00:51:24 +0200

    Added a ViewModel class

---
+++ app/src/main/java/hochschule/de/bachelorthesis/view_model/activities/AddMeasurementViewModel.java
@@ -0,0 +1,22 @@
+package hochschule.de.bachelorthesis.view_model.activities;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import hochschule.de.bachelorthesis.model.Repository;
+import hochschule.de.bachelorthesis.room.tables.Food;
+
+public class AddMeasurementViewModel extends AndroidViewModel {
+    private Repository repository;
+
+    public AddMeasurementViewModel(@NonNull Application application) {
+        super(application);
+        repository = new Repository(application);
+    }
+
+    public void insert(Food food) {
+        repository.insert(food);
+    }
+}
