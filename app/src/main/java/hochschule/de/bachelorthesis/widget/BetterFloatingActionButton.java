4195e0f5434af56e1704b87f1be1dd41b16b6c17
Kailyou <maik.thielen@gmail.com>
2019-05-21 00:47:32 +0200

    Created own FloatingActionButton class
    - This is needed to get the icon color white

---
+++ app/src/main/java/hochschule/de/bachelorthesis/widget/BetterFloatingActionButton.java
@@ -0,0 +1,30 @@
+package hochschule.de.bachelorthesis.widget;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+
+import androidx.annotation.Nullable;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class BetterFloatingActionButton extends FloatingActionButton {
+    public BetterFloatingActionButton(Context context) {
+        super(context);
+        construct();
+    }
+
+    public BetterFloatingActionButton(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        construct();
+    }
+
+    public BetterFloatingActionButton(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        construct();
+    }
+
+    private void construct() {
+        getDrawable().mutate().setTint(Color.WHITE);
+    }
+}
