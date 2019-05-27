5d2996ab74c468f7b251511a647d1b974f7b6b09
Kailyou <maik.thielen@gmail.com>
2019-05-27 06:29:56 +0200

    Design Changes

--- app/src/main/java/hochschule/de/bachelorthesis/view/fragments/mainActivity/MeFragment.java
+++ app/src/main/java/hochschule/de/bachelorthesis/view/fragments/mainActivity/MeFragment.java
@@ -1,6 +1,7 @@
 package hochschule.de.bachelorthesis.view.fragments.mainActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -25,8 +26,7 @@ import hochschule.de.bachelorthesis.databinding.FragmentMeBinding;
 
 public class MeFragment extends Fragment {
     private MeViewModel mViewModel;
-    private FragmentMeBinding mBinding;
-    BaseObservable x;
+
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -42,63 +42,18 @@ public class MeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Init data binding
-        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_me, container, false);
-        mBinding.setLifecycleOwner(getViewLifecycleOwner());
-        mBinding.setVm(mViewModel);
+        FragmentMeBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_me, container, false);
+        binding.setLifecycleOwner(getViewLifecycleOwner());
+        binding.setVm(mViewModel);
 
-        mBinding.buttonEditMe.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_main_activity_me_fragment_to_meEditFragment));
+        binding.buttonEditMe.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_main_activity_me_fragment_to_meEditFragment));
 
-
-        return mBinding.getRoot();
+        return binding.getRoot();
     }
 
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.me_menu, menu);
-    }
-
-    /**
-     * This function is supposed to load the current user history data from the database.
-     * For test reasons, it will now just set the data.
-     * TODO implement database function
-     */
-    private void loadUser() {
-
-    }
-
-    /**
-     * Updates the view model.
-     */
-    private void updateViewModel() {
-        /*
-        Integer age = Integer.parseInt(Objects.requireNonNull(mBinding.age.getText()).toString());
-        Integer height = Integer.parseInt(Objects.requireNonNull(mBinding.height.getText()).toString());
-        Integer weight = Integer.parseInt(Objects.requireNonNull(mBinding.weight.getText()).toString());
-        String sex = mBinding.dropdownSex.getText().toString();
-        String fitnessLevel = mBinding.dropdownFitnessLevel.getText().toString();
-        boolean medication = false;
-        boolean allergies = false;
-        boolean smoking = false;
-
-        // Checkboxes
-        if(mBinding.medication.isChecked()) {
-            medication = true;
-        }
-
-        if(mBinding.allergies.isChecked()) {
-            allergies = true;
-        }
-
-        if(mBinding.smoking.isChecked()) {
-            smoking = true;
-        }
-
-        mViewModel.update(age, height, weight, sex, fitnessLevel, medication, allergies, smoking);
-        */
-    }
-
-    private void toast(String msg) {
-        MyToast.createToast(getContext(), msg);
     }
 }
