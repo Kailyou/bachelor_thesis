3ed56449c97b4e5fc0eea698c7c5deef6b6b8e1c
Kailyou <maik.thielen@gmail.com>
2019-07-06 13:04:44 +0200

    Removed unused imports

--- app/src/main/java/hochschule/de/bachelorthesis/utility/MySnackBar.java
+++ app/src/main/java/hochschule/de/bachelorthesis/utility/MySnackBar.java
@@ -3,7 +3,6 @@ package hochschule.de.bachelorthesis.utility;
 import android.app.Activity;
 import android.content.Context;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.snackbar.Snackbar;
 
 public class MySnackBar {
   public static void createSnackBar(Context context, String text) {
