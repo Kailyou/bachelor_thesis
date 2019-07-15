ef5467ba5bcb4078d50ff5ab32beee24073a906f
Kailyou <maik.thielen@gmail.com>
2019-07-15 15:05:21 +0200

    Removed unused model data - Those data will be calculated anyway

--- app/src/main/java/hochschule/de/bachelorthesis/model/FoodOverviewModel.java
+++ app/src/main/java/hochschule/de/bachelorthesis/model/FoodOverviewModel.java
@@ -13,30 +13,12 @@ public class FoodOverviewModel {
   private MutableLiveData<String> mType;
   private MutableLiveData<Float> mKiloCalories;
 
-  // Measurements
-  private MutableLiveData<Integer> mMeasurementsAmount;
-  private MutableLiveData<Integer> mMaxGlucose;
-  private MutableLiveData<Integer> mAverageGlucose;
-  private MutableLiveData<Integer> mIntegral;
-  private MutableLiveData<Integer> mStdev;
-
-  // Analyses
-  private MutableLiveData<String> mRating;
-  private MutableLiveData<Integer> mPersonalIndex;
-
   public FoodOverviewModel() {
     mIsFavorite = new MutableLiveData<>();
     mFoodName = new MutableLiveData<>();
     mBrandName = new MutableLiveData<>();
     mType = new MutableLiveData<>();
     mKiloCalories = new MutableLiveData<>();
-    mMeasurementsAmount = new MutableLiveData<>();
-    mMaxGlucose = new MutableLiveData<>();
-    mAverageGlucose = new MutableLiveData<>();
-    mIntegral = new MutableLiveData<>();
-    mStdev = new MutableLiveData<>();
-    mRating = new MutableLiveData<>();
-    mPersonalIndex = new MutableLiveData<>();
   }
 
   /* GETTER */
@@ -59,34 +41,6 @@ public class FoodOverviewModel {
 
   public MutableLiveData<Float> getKiloCalories() {
     return mKiloCalories;
-  }
-
-  public MutableLiveData<Integer> getMeasurementsAmount() {
-    return mMeasurementsAmount;
-  }
-
-  public MutableLiveData<Integer> getMaxGlucose() {
-    return mMaxGlucose;
-  }
-
-  public MutableLiveData<Integer> getAverageGlucose() {
-    return mAverageGlucose;
-  }
-
-  public MutableLiveData<Integer> getIntegral() {
-    return mIntegral;
-  }
-
-  public MutableLiveData<Integer> getStdev() {
-    return mStdev;
-  }
-
-  public MutableLiveData<String> getRating() {
-    return mRating;
-  }
-
-  public MutableLiveData<Integer> getPersonalIndex() {
-    return mPersonalIndex;
   }
 
   /* SETTER */
