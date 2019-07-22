3ca364f8f97327f5fdb6519539551ffb85c54e89
Kailyou <maik.thielen@gmail.com>
2019-07-22 08:04:17 +0200

    Added data binding and menu

--- app/src/main/java/hochschule/de/bachelorthesis/view/home/HomeFragment.java
+++ app/src/main/java/hochschule/de/bachelorthesis/view/home/HomeFragment.java
@@ -2,29 +2,70 @@ package hochschule.de.bachelorthesis.view.home;
 
 import android.os.Bundle;
 
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
+import hochschule.de.bachelorthesis.databinding.FragmentMeBinding;
+import hochschule.de.bachelorthesis.viewmodels.HomeViewModel;
+import hochschule.de.bachelorthesis.viewmodels.MeViewModel;
 import java.util.Objects;
 
 import hochschule.de.bachelorthesis.R;
 
 public class HomeFragment extends Fragment {
 
+  private HomeViewModel mViewModel;
+
+  private FragmentMeBinding mBinding;
+
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    // Change title
+    Objects.requireNonNull(getActivity()).setTitle("Home");
+
+    // Enable menu
+    setHasOptionsMenu(true);
+
+    // View model
+    mViewModel = ViewModelProviders.of(getActivity()).get(HomeViewModel.class);
   }
 
   @Nullable
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_home, container, false);
+
+    // Init data binding
+    mBinding = DataBindingUtil
+        .inflate(inflater, R.layout.fragment_me, container, false);
+    mBinding.setLifecycleOwner(this);
+
+    return mBinding.getRoot();
+  }
+
+
+  @Override
+  public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+    super.onCreateOptionsMenu(menu, inflater);
+    inflater.inflate(R.menu.home_menu, menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+    return super.onOptionsItemSelected(item);
   }
 }
 
