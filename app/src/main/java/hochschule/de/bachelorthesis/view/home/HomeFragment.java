fac570ac8fc2f36b17f75bc2ad74048bedd35b09
Kailyou <maik.thielen@gmail.com>
2019-07-22 08:24:46 +0200

    Displays current active measurement

--- app/src/main/java/hochschule/de/bachelorthesis/view/home/HomeFragment.java
+++ app/src/main/java/hochschule/de/bachelorthesis/view/home/HomeFragment.java
@@ -14,11 +14,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.Navigation;
+import hochschule.de.bachelorthesis.databinding.FragmentHomeBinding;
 import hochschule.de.bachelorthesis.databinding.FragmentMeBinding;
+import hochschule.de.bachelorthesis.room.tables.Food;
+import hochschule.de.bachelorthesis.room.tables.Measurement;
+import hochschule.de.bachelorthesis.utility.Converter;
 import hochschule.de.bachelorthesis.viewmodels.HomeViewModel;
 import hochschule.de.bachelorthesis.viewmodels.MeViewModel;
+import java.util.List;
 import java.util.Objects;
 
 import hochschule.de.bachelorthesis.R;
@@ -27,7 +34,7 @@ public class HomeFragment extends Fragment {
 
   private HomeViewModel mViewModel;
 
-  private FragmentMeBinding mBinding;
+  private FragmentHomeBinding mBinding;
 
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -49,8 +56,10 @@ public class HomeFragment extends Fragment {
 
     // Init data binding
     mBinding = DataBindingUtil
-        .inflate(inflater, R.layout.fragment_me, container, false);
+        .inflate(inflater, R.layout.fragment_home, container, false);
     mBinding.setLifecycleOwner(this);
+
+    loadActiveMeasurement();
 
     return mBinding.getRoot();
   }
@@ -66,6 +75,40 @@ public class HomeFragment extends Fragment {
   public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
     return super.onOptionsItemSelected(item);
+  }
+
+  private void loadActiveMeasurement() {
+
+    // Load all measurements
+    final LiveData<List<Measurement>> ldMeasurements = mViewModel.getAllMeasurements();
+    ldMeasurements.observe(getViewLifecycleOwner(),
+        new Observer<List<Measurement>>() {
+          @Override
+          public void onChanged(List<Measurement> measurements) {
+            ldMeasurements.removeObserver(this);
+
+            for (final Measurement measurement : measurements) {
+              if (measurement.isActive()) {
+
+                // Load food object
+                final LiveData<Food> ldFood = mViewModel.getFoodById(measurement.getFoodId());
+                ldFood.observe(getViewLifecycleOwner(), new Observer<Food>() {
+                  @Override
+                  public void onChanged(Food food) {
+                    ldFood.removeObserver(this);
+
+                    mBinding.foodName.setText(food.getFoodName());
+                    mBinding.timeStarted
+                        .setText(Converter.convertTimeStampToTimeStart(measurement.getTimeStamp()));
+                    mBinding.timeEnded
+                        .setText(Converter.convertTimeStampToTimeEnd(measurement.getTimeStamp()));
+                    mBinding.interval.setText(String.valueOf(15));
+                  }
+                });
+              }
+            }
+          }
+        });
   }
 }
 
--- app/src/main/java/hochschule/de/bachelorthesis/viewmodels/HomeViewModel.java
+++ app/src/main/java/hochschule/de/bachelorthesis/viewmodels/HomeViewModel.java
@@ -5,7 +5,10 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import hochschule.de.bachelorthesis.model.Repository;
+import hochschule.de.bachelorthesis.room.tables.Food;
+import hochschule.de.bachelorthesis.room.tables.Measurement;
 import hochschule.de.bachelorthesis.room.tables.UserHistory;
+import java.util.List;
 
 public class HomeViewModel extends AndroidViewModel {
 
@@ -14,5 +17,19 @@ public class HomeViewModel extends AndroidViewModel {
   public HomeViewModel(@NonNull Application application) {
     super(application);
     mRepository = new Repository(application);
+  }
+
+  /**
+   * Gets the food by ID.
+   *
+   * @param id - The food's id.
+   * @return - A live data object with the food.
+   */
+  public LiveData<Food> getFoodById(int id) {
+    return mRepository.getFoodById(id);
+  }
+
+  public LiveData<List<Measurement>> getAllMeasurements() {
+    return mRepository.getAllMeasurements();
   }
 }
